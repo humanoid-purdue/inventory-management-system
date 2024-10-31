9870d211389d03a4f4fd9173f6f0d18800d2de03
deepanshsaxena <deepanshs@live.com>
2024-10-31 00:21:12 -0400

    Setup flutter and created and programmed whole login page.

---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 140.0,
+            ),
+            Container(
+              child: Center(child: Text("Inventory Management System", style: TextStyle(fontSize: 45,color: Color.fromRGBO(0, 0, 128, 10)),)),
+            ),
+            Container(child: Image.asset('/Users/deepanshsaxena/AndroidStudioProjects/inventory_management_system/lib/hrc.png',height: 300,width: 300,),),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: InputDecoration(filled: true,fillColor: Colors.white60,
+                  hintText: 'Enter your email',
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.black, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.black, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: InputDecoration(filled: true,fillColor: Colors.white60,
+                  hintText: 'Enter your password',
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.black, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.black, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 42.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 2.0),
+              child: Material(
+                color: Color.fromRGBO(255, 188, 33, 10),
+                borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    //Implement login functionality.
+                  },
+                  minWidth: 330.0,
+                  height: 42.0,
+                  child: Text(style: TextStyle(color: Color.fromRGBO(0, 0, 128, 10)),
+                    'Log In',
+                  ),
+                ),
+              ),
+            ), SizedBox(
+              height: 24.0,
+            ),
+          ],
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+        ),
+      ),
+    );
+  }
+}
